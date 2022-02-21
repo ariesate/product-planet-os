5d43d43468356200a6454725ff5b55300fd62806
fanglin05 <fanglin05@kuaishou.com>
2022-02-21 11:47:13 +0800

    feat: github config

--- packages/product-planet-server/config/default.js
+++ packages/product-planet-server/config/default.js
@@ -15,6 +15,15 @@ export default {
     accessToken: 'VrVM1q2Hhf_ai4Ddob2s',
     namespaceId: 21132
   },
+  gitHub: {
+    clientId: '5db6ae35e220a33ab432',
+    clientSecret: 'e8e714d02d5d89ad8ba21a9159e655cd5a055b0e',
+    appName: 'product-planet',
+    authUrl: 'https://github.com/login/oauth/authorize',
+    authTokenUrl: 'https://github.com/login/oauth/access_token',
+    homePage: 'http://product-planet-local.staging.kuaishou.com:4001',
+    backPage: 'http://product-planet-local.staging.kuaishou.com:4001'
+  },
   moduleConfig: {
     storage: {
       enable: true,
