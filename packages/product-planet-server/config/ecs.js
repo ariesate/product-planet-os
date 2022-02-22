479d026b1469a998554276bb549b2f7a9fd049e9
Sean Cheung <theoxuanx@gmail.com>
2022-02-22 16:52:10 +0800

    fix: fix missing config field

--- packages/product-planet-server/config/ecs.js
+++ packages/product-planet-server/config/ecs.js
@@ -18,6 +18,15 @@ export default {
     accessToken: 'VrVM1q2Hhf_ai4Ddob2s',
     namespaceId: 21132
   },
+  github: {
+    clientId: '5db6ae35e220a33ab432',
+    clientSecret: 'e8e714d02d5d89ad8ba21a9159e655cd5a055b0e',
+    appName: 'product-planet',
+    authUrl: 'https://github.com/login/oauth/authorize',
+    authTokenUrl: 'https://github.com/login/oauth/access_token',
+    homePage: 'http://localhost:8080/github',
+    backPage: 'http://localhost:8080/github'
+  },
   moduleConfig: {
     storage: {
       enable: true,
