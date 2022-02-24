54a2c3f539568800dfd9e8acc96b1c839aabe6d2
fanglin05 <fanglin05@kuaishou.com>
2022-02-24 14:14:46 +0800

    feat: feat/move

---
+++ packages/product-planet-website/components/ProtoDraftEditor/util.js
@@ -0,0 +1,14 @@
+import api from '@/services/api'
+
+export function getImageFromSrc (src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.onload = () => {
+      resolve(img)
+    }
+    img.onerror = (e) => {
+      reject(e)
+    }
+    img.src = src
+  })
+}
