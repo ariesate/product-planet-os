4ae56d9fc046bddba0f8601d1731b3d5af322952
yangyang23 <yangyang23@kuaishou.com>
2022-02-25 16:37:18 +0800

    feat(ProtoEditor): support to show markup default

--- packages/product-planet-website/pages/page-editor/index.jsx
+++ packages/product-planet-website/pages/page-editor/index.jsx
@@ -1,4 +1,4 @@
-import { createElement, atomComputed, atom } from 'axii'
+import { createElement, atomComputed, computed } from 'axii'
 import ProtoEditor from '@/components/ProtoEditor'
 import { historyLocation } from '@/router'
 import parseSearch from '@/tools/parseSearch'
@@ -6,10 +6,14 @@ import parseSearch from '@/tools/parseSearch'
 export default () => {
   const pageId = location.pathname.split('/').pop()
   const onStatusSelect = (id) => historyLocation.goto(`${historyLocation.pathname}?layout=hidden&status=${id}`)
-  const status = atomComputed(() => {
+  const statusId = atomComputed(() => {
     const search = parseSearch(historyLocation.search)
-    console.log('compute status', search.status)
     return +search.status
   })
-  return <ProtoEditor pageId={pageId} statusId={status} onStatusSelect={onStatusSelect} />
+
+  const pinId = computed(() => {
+    const search = parseSearch(historyLocation.search)
+    return search.pin
+  })
+  return <ProtoEditor pageId={pageId} statusId={statusId} pinId={pinId} onStatusSelect={onStatusSelect} />
 }
