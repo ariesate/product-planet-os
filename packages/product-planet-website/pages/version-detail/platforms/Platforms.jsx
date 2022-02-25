b8e8abe5bf08c6c5a004cee01056490b067d0f4d
zhangxuan11 <zhangxuan11@kuaishou.com>
2022-02-25 13:45:18 +0800

    fix: fix product detail render issue

--- packages/product-planet-website/pages/version-detail/platforms/Platforms.jsx
+++ packages/product-planet-website/pages/version-detail/platforms/Platforms.jsx
@@ -10,16 +10,20 @@ import {
 import ButtonNew from '@/components/Button.new'
 import Gitlab from './Gitlab'
 import styles from '../style.module.less'
+import { useVersion } from '@/layouts/VersionLayout'
 
 function Platforms () {
+  const version = useVersion()
   return (
         <div className={styles.block}>
           <div className={styles.title}>
             <div className={styles.lf}>关联平台</div>
           </div>
-          <content block flex-display flex-direction-row>
+          {() => !version.value
+            ? null
+            : <content block flex-display flex-direction-row>
             <Gitlab />
-          </content>
+          </content>}
         </div>
   )
 }
