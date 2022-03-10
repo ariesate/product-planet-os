ee609ca5ee0b8b3dc856b64c00feb297f6e187fc
fanglin05 <fanglin05@kuaishou.com>
2022-03-10 10:49:51 +0800

    feat: github auth app

--- packages/product-planet-server/config/default.js
+++ packages/product-planet-server/config/default.js
@@ -19,9 +19,9 @@ export default {
     host: 'https://test-product-planet.oss-cn-hangzhou.aliyuncs.com'
   },
   github: {
-    clientId: '5db6ae35e220a33ab432',
-    clientSecret: 'e8e714d02d5d89ad8ba21a9159e655cd5a055b0e',
-    appName: 'product-planet',
+    clientId: '434e012483fc722007e8',
+    clientSecret: '1a6e25a2153df9dd6d97b32ed5f6944607fd51b3',
+    appName: 'product-planet-dev',
     authUrl: 'https://github.com/login/oauth/authorize',
     authTokenUrl: 'https://github.com/login/oauth/access_token',
     homePage: 'http://localhost:8080/github',
